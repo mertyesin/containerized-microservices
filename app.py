ab2a1ad2df74e700e0f6e27df7bfdbb56331074f
Mert Yesin <mert.yesin@accenture.com>
2018-11-09 13:56:54 +0300

    minor conf

--- app.py
+++ app.py
@@ -18,89 +18,65 @@ def add_contact():
         data = json.loads(request.data)
         user_name = data['name']
         user_contact = data['contact']
+        '''POST Body should be like this:
+        {
+            "name": "Ferenc",
+            "contact": "122222313111"
+        }
+        If user entered both required fields,then
+        we add record to DB for this user '''
         if user_name and user_contact:
             status = db.Contacts.insert_one({
                 "name" : user_name,
                 "contact" : user_contact
             })
-        return dumps({'message' : 'SUCCESS'})
+            return dumps({'message' : 'Succesfully recorded to DB'})
+        else:
+            return dumps({'message' : 'Record not created'})
     except Exception, e:
         return dumps({'error' : str(e)})
 
 
-@app.route("/get_contact/", methods = ['GET'])
+@app.route("/get_contact", methods = ['GET'])
 def get_contact():
-    s = db.Contacts.find_one({'name' : "repo"})
+    try:
+        if request.args:
+            name = request.args
+            data = db.Contacts.find_one(name)
+            ''' If user entered a specific name like this:
+            http://localhost:5000/get_contact?name=Ferenc
+            we get record for this user '''
+            if data:   
+                return dumps(data)
+            else:
+                return dumps({'message' : 'Record not found'})
+        else:
+            ''' If user not entered a specific name like this:
+            http://localhost:5000/get_contact
+            we get all records from Contacts DB '''
+            data = db.Contacts.find()
+            return dumps(data)
 
-    if s:
-      output = {'name' : s['name'], 'contact' : s['contact']}
-    else:
-      output = "No such name"
-    return jsonify({'result' : output})
+    except Exception, e:
+        return dumps({'error' : str(e)})
 
+@app.route("/delete_contact", methods = ['DELETE'])
+def delete_contact():
+    try:
+        if request.args:
+            name = request.args
+            data = db.Contacts.find_one(name)
+            ''' If user entered a specific name like this:
+            http://localhost:5000/delete_contact?name=Ferenc
+            we delete this record from DB'''
+            if data:
+                db.Contacts.delete_one(name)   
+                return dumps({'message' : 'Record deleted'})
+            else:
+                return dumps({'message' : 'Record not found'})
 
-# from flask import Flask
-# from flask import jsonify
-# from flask import request
-# from flask_pymongo import PyMongo
-# from bson.json_util import dumps
-# import json
-
-# client = MongoClient('localhost:27017')
-# db = client.ContactDB
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def hello_world():
-#     return 'Flask Dockerized'
-
-
-# mongo = PyMongo(app)
-
-# @app.route('/star', methods=['GET'])
-# def get_all_stars():
-#   print ("get_all_stars")
-#   star = mongo.db.stars
-#   output = [
-#         {
-#             'name':'Mert',
-#             'distance': '15245'
-#         }
-#   ]
-#   # for s in star.find():
-#   #   output.append({'name' : s['name'], 'distance' : s['distance']})
-#   return jsonify({'result' : output})
-
-# @app.route("/add_contact", methods = ['POST'])
-# def add_contact():
-#     try:
-#         data = json.loads(request.data)
-#         user_name = data['name']
-#         user_contact = data['contact']
-#         if user_name and user_contact:
-#             status = db.Contacts.insert_one({
-#                 "name" : user_name,
-#                 "contact" : user_contact
-#             })
-#         return dumps({'message' : 'SUCCESS'})
-
-#     except Exception, e:
-#         return dumps({'error' : str(e)})
+    except Exception, e:
+        return dumps({'error' : str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
-
-# # @app.route('/star/', methods=['GET'])
-# # def get_one_star(name):
-# #   star = mongo.db.stars
-# #   s = star.find_one({'name' : name})
-# #   if s:
-# #     output = {'name' : s['name'], 'distance' : s['distance']}
-# #   else:
-# #     output = "No such name"
-# #   return jsonify({'result' : output})
-
-
-# # if __name__ == '__main__':
-# #     app.run(debug=True)
